d1d19faaac1898ffb9033f36bafa9d5b8c73f69c
Qui <lquiitfuture@gmail.com>
2019-12-18 23:52:23 +0700

    change Program.cs file

--- Program.cs
+++ Program.cs
@@ -7,9 +7,12 @@ namespace DigitSeparators
         static void Main(string[] args)
         {
             // Both are equivalent.
-            var bigNumber = 123456789012345678;
-            var bigNumberSplit = 123_456_789_012_345_678;
-            Console.WriteLine("bigNumber : {0}, bigNumberSplit : {1}", bigNumber, bigNumberSplit);
+            // var bigNumber = 123456789012345678;
+            // var bigNumberSplit = 123_456_789_012_345_678;
+            var myData1 = 1__2___3__4_______5_______6;
+            var myData2 = 1__2__3_____4_5________6.79;
+            Console.WriteLine("bigNumber : {0}, bigNumberSplit : {1}", myData1, 
+                myData2);
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
